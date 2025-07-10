cee146f00e43ce9eb7c6da41284a3781672d65cc
kimura5211 <r202304929fb@jindai.jp>
2025-07-10 22:55:00 +0900

    Update missile.rkt

--- missile.rkt
+++ missile.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (require 2htdp/image
-         "enemy.rkt")
+         "enemys.rkt") ; 複数の敵に対応したファイルを読み込む
 
 (provide missile-x missile-y
          draw-missiles next-missiles move-missile
@@ -9,27 +9,27 @@
 (define MISSILE (circle 10 "solid" "red"))
 (define SCENE-WIDTH 400)
 (define SCENE-HEIGHT 600)
-;; 循環回避のため敵サイズを定義
-(define ENEMY-SIZE 40)
+(define ENEMY-SIZE 40) ; 敵サイズ（敵ファイルと整合性を保つため定義）
 
 ;; ミサイル位置取得
 (define (missile-x m) (car m))
 (define (missile-y m) (cadr m))
 
-;; ミサイル1つの移動/弾のスピードを引数で調整可能に
+;; ミサイル1つの移動
 (define (move-missile m speed)
   (list (missile-x m) (- (missile-y m) speed)))
 
-;; next-missiles の引数にレベルを追加して調整
-(define (next-missiles missiles e level)
-  (filter (lambda (m) (missile-alive? (move-missile m (+ 8 (* level 2))) e))
-          (map (lambda (m) (move-missile m (+ 8 (* level 2)))) missiles)))
+;; 敵リストに対応した next-missiles
+(define (next-missiles missiles enemies level)
+  (define speed (+ 8 (* level 2)))
+  (filter (lambda (m)
+            (missile-alive? (move-missile m speed) enemies))
+          (map (lambda (m) (move-missile m speed)) missiles)))
 
-;; ミサイルが画面内で、かつ敵に当たっていないかどうか
-(define (missile-alive? m e)
+;; 敵リストに対応した生存チェック
+(define (missile-alive? m enemies)
   (and (>= (missile-y m) 0)
-       (not (string? e))
-       (not (hit? m e))))
+       (not (any-hit? (list m) enemies)))) ; 1発と全敵の判定
 
 ;; 全ミサイルを描画
 (define (draw-missiles missiles)
@@ -38,12 +38,15 @@
          (empty-scene SCENE-WIDTH SCENE-HEIGHT)
          missiles))
 
-;; hit?,any-hit?定義
+;; 単体の敵とミサイルの衝突判定
 (define (hit? m e)
   (and (not (string? m))
        (not (string? e))
        (< (abs (- (car m) (car e))) (/ ENEMY-SIZE 2))
        (< (abs (- (cadr m) (cadr e))) (/ ENEMY-SIZE 2))))
 
-(define (any-hit? missiles e)
-  (ormap (lambda (m) (hit? m e)) missiles))
+;; 敵リストに対して、ミサイルがどれかに当たったか
+(define (any-hit? missiles enemies)
+  (ormap (lambda (e)
+           (ormap (lambda (m) (hit? m e)) missiles))
+         enemies))
